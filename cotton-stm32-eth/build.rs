cc49efa00b3bca6cecbbe23686774e7043db2ece
Peter Hartley <peter@electricimp.com>
2023-02-01 15:21:17 +0000

    Fix build with separate test package
    Now works on both stable and nightly!
    
        cargo build
        cargo test
        cargo build -F arm #<-- needs thumbv7em-none-eabi toolchain installed
        cargo test -F arm
        cargo build -F arm,stm32f7-nucleo
        cargo test -F arm,stm32f7-nucleo   #<-- uses attached Nucleo

--- cotton-stm32-eth/build.rs
+++ cotton-stm32-eth/build.rs
@@ -2,19 +2,24 @@ use std::collections::HashMap;
 use std::env;
 use std::process::Command;
 
+use std::io::{self, Write};
+
 fn main() {
-    println!("cargo:rerun-if-changed=cross-stm32f7-nucleo");
+    println!("cargo:rerun-if-changed=../cross-stm32f7-nucleo");
 
     if env::var("CARGO_FEATURE_ARM").is_ok() {
         /* Run the inner Cargo without any Cargo-related environment variables
          * from this outer Cargo.
          */
+for (key, value) in env::vars() {
+    println!("{key}: {value}");
+}
         let filtered_env: HashMap<String, String> = env::vars()
             .filter(|(k, _)| !k.starts_with("CARGO"))
             .collect();
-
-        let _child = Command::new("cargo")
+        let child = Command::new("cargo")
             .arg("build")
+            .arg("-vv")
             .arg("--all-targets")
             .arg("--target")
             .arg("thumbv7em-none-eabi")
@@ -23,7 +28,10 @@ fn main() {
             .current_dir("../cross-stm32f7-nucleo")
             .env_clear()
             .envs(&filtered_env)
-            .status()
+            .output()
             .expect("failed to cross-compile for ARM");
+        io::stdout().write_all(&child.stderr).unwrap();
+        io::stdout().write_all(&child.stdout).unwrap();
+        assert!(child.status.success());
     }
 }
--- cotton-stm32-eth/tests/stm32f7-nucleo.rs
+++ cotton-stm32-eth/tests/stm32f7-nucleo.rs
@@ -1,5 +1,6 @@
 use serial_test::*;
 use std::process::Command;
+use std::path::Path;
 
 use std::io::{self, Write};
 
@@ -11,14 +12,22 @@ fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
 #[serial]
 #[cfg_attr(miri, ignore)]
 fn arm_stm32f7_hello() {
+    let elf = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("../cross-stm32f7-nucleo/target-arm/thumbv7em-none-eabi/debug/hello");
+    
     let output = Command::new("probe-run")
         .arg("--chip")
         .arg("STM32F746ZGTx")
-        .arg("../cross-stm32f7-nucleo/target-arm/thumbv7em-none-eabi/debug/hello")
+        .arg(elf)
         .output()
-        .expect("failed to flash STM32F7 Nucleo");
+        .expect("failed to execute probe-run");
 
+    println!("manifest: {}", env!("CARGO_MANIFEST_DIR"));
+    println!("status: {}", output.status);
+    io::stdout().write_all(&output.stderr).unwrap();
     io::stdout().write_all(&output.stdout).unwrap();
+    assert!(output.status.success());
 
     assert!(find_subsequence(&output.stdout, b"Hello world").is_some());
 }
+
