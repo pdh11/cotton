25b586781b562375c4062aa60304a75d61106dd7
Peter Hartley <peter@electricimp.com>
2023-02-03 17:56:16 +0000

    Cargo fmt

--- cotton-stm32-eth/tests/stm32f7-nucleo.rs
+++ cotton-stm32-eth/tests/stm32f7-nucleo.rs
@@ -1,20 +1,23 @@
 use serial_test::*;
-use std::process::Command;
 use std::path::Path;
+use std::process::Command;
 
 use std::io::{self, Write};
 
 fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
-    haystack.windows(needle.len()).position(|window| window == needle)
+    haystack
+        .windows(needle.len())
+        .position(|window| window == needle)
 }
 
 #[test]
 #[serial]
 #[cfg_attr(miri, ignore)]
 fn arm_stm32f7_hello() {
-    let elf = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("../cross-stm32f7-nucleo/target-arm/thumbv7em-none-eabi/debug/hello");
-    
+    let elf = Path::new(env!("CARGO_MANIFEST_DIR")).join(
+        "../cross-stm32f7-nucleo/target-arm/thumbv7em-none-eabi/debug/hello",
+    );
+
     let output = Command::new("probe-run")
         .arg("--chip")
         .arg("STM32F746ZGTx")
@@ -30,4 +33,3 @@ fn arm_stm32f7_hello() {
 
     assert!(find_subsequence(&output.stdout, b"Hello world").is_some());
 }
-
