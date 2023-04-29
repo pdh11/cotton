33a8400676d76c79f07179050ba2b5327adb67ba
Peter Hartley <pdh@utter.chaos.org.uk>
2023-04-29 12:37:17 +0100

    Rejig directories again
    Now all the cross-* directories are under "cross" (because there might one
    day be a lot of them). And there's a new "systemtests" directory because
    cotton-ssdp, for example, shouldn't need to know about all the different
    devboard in use -- i.e., adding a new devboard shouldn't involve changing
    cotton-ssdp.

---
+++ cross/rp2040/src/bin/hello.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _; // global logger
+use panic_probe as _;
+use cortex_m::asm;
+use rp_pico as _; // includes boot2
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    defmt::println!("Hello RP2040!");
+
+    loop {
+        asm::bkpt()
+    }
+}
