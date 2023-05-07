e1d87154f94c51a0f85062f991ad4462b8eb3858
Peter Hartley <pdh@utter.chaos.org.uk>
2023-05-07 11:06:21 +0100

    Remove phy autodetection
    All Nucleo-F746 boards have LAN8742A phy populated.

--- cross/stm32f746-nucleo/src/bin/dhcp-rtic.rs
+++ cross/stm32f746-nucleo/src/bin/dhcp-rtic.rs
@@ -4,11 +4,8 @@
 use defmt_rtt as _; // global logger
 use panic_probe as _;
 use stm32f7xx_hal as _;
-
 use smoltcp::iface::{self, SocketStorage};
-
 use stm32_eth::hal::rcc::Clocks;
-
 use fugit::RateExtU32;
 use stm32_eth::hal::rcc::RccExt;
 
@@ -18,137 +15,22 @@ pub fn setup_clocks(rcc: stm32_eth::stm32::RCC) -> Clocks {
     rcc.cfgr.sysclk(100.MHz()).hclk(100.MHz()).freeze()
 }
 
-use ieee802_3_miim::{
-    phy::{
-        lan87xxa::{LAN8720A, LAN8742A},
-        BarePhy, KSZ8081R,
-    },
-    Miim, Pause, Phy,
-};
-
-/// An ethernet PHY
-pub enum EthernetPhy<M: Miim> {
-    /// LAN8720A
-    LAN8720A(LAN8720A<M>),
-    /// LAN8742A
-    LAN8742A(LAN8742A<M>),
-    /// KSZ8081R
-    KSZ8081R(KSZ8081R<M>),
-}
-
-impl<M: Miim> Phy<M> for EthernetPhy<M> {
-    fn best_supported_advertisement(
-        &self,
-    ) -> ieee802_3_miim::AutoNegotiationAdvertisement {
-        match self {
-            EthernetPhy::LAN8720A(phy) => phy.best_supported_advertisement(),
-            EthernetPhy::LAN8742A(phy) => phy.best_supported_advertisement(),
-            EthernetPhy::KSZ8081R(phy) => phy.best_supported_advertisement(),
-        }
-    }
-
-    fn get_miim(&mut self) -> &mut M {
-        match self {
-            EthernetPhy::LAN8720A(phy) => phy.get_miim(),
-            EthernetPhy::LAN8742A(phy) => phy.get_miim(),
-            EthernetPhy::KSZ8081R(phy) => phy.get_miim(),
-        }
-    }
-
-    fn get_phy_addr(&self) -> u8 {
-        match self {
-            EthernetPhy::LAN8720A(phy) => phy.get_phy_addr(),
-            EthernetPhy::LAN8742A(phy) => phy.get_phy_addr(),
-            EthernetPhy::KSZ8081R(phy) => phy.get_phy_addr(),
-        }
-    }
-}
-
-impl<M: Miim> EthernetPhy<M> {
-    /// Attempt to create one of the known PHYs from the given
-    /// MIIM.
-    ///
-    /// Returns an error if the PHY does not support the extended register
-    /// set, or if the PHY's identifier does not correspond to a known PHY.
-    pub fn from_miim(miim: M, phy_addr: u8) -> Result<Self, M> {
-        let mut bare = BarePhy::new(miim, phy_addr, Pause::NoPause);
-        let phy_ident = if let Some(id) = bare.phy_ident() {
-            id.raw_u32()
-        } else {
-            return Err(bare.release());
-        };
-        let miim = bare.release();
-        match phy_ident & 0xFFFFFFF0 {
-            0x0007C0F0 => Ok(Self::LAN8720A(LAN8720A::new(miim, phy_addr))),
-            0x0007C130 => Ok(Self::LAN8742A(LAN8742A::new(miim, phy_addr))),
-            0x00221560 => Ok(Self::KSZ8081R(KSZ8081R::new(miim, phy_addr))),
-            _ => Err(miim),
-        }
-    }
-
-    /// Get a string describing the type of PHY
-    pub const fn ident_string(&self) -> &'static str {
-        match self {
-            EthernetPhy::LAN8720A(_) => "LAN8720A",
-            EthernetPhy::LAN8742A(_) => "LAN8742A",
-            EthernetPhy::KSZ8081R(_) => "KSZ8081R",
-        }
-    }
-
-    /// Initialize the PHY
-    pub fn phy_init(&mut self) {
-        match self {
-            EthernetPhy::LAN8720A(phy) => phy.phy_init(),
-            EthernetPhy::LAN8742A(phy) => phy.phy_init(),
-            EthernetPhy::KSZ8081R(phy) => {
-                phy.set_autonegotiation_advertisement(
-                    phy.best_supported_advertisement(),
-                );
-            }
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn speed(&mut self) -> Option<ieee802_3_miim::phy::PhySpeed> {
-        match self {
-            EthernetPhy::LAN8720A(phy) => phy.link_speed(),
-            EthernetPhy::LAN8742A(phy) => phy.link_speed(),
-            EthernetPhy::KSZ8081R(phy) => phy.link_speed(),
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn release(self) -> M {
-        match self {
-            EthernetPhy::LAN8720A(phy) => phy.release(),
-            EthernetPhy::LAN8742A(phy) => phy.release(),
-            EthernetPhy::KSZ8081R(phy) => phy.release(),
-        }
-    }
-}
-
 #[rtic::app(device = stm32_eth::stm32, dispatchers = [SPI1])]
 mod app {
-
-    use super::EthernetPhy;
-
     use ieee802_3_miim::{phy::PhySpeed, Phy};
     use systick_monotonic::Systick;
-
     use stm32_eth::{
         dma::{EthernetDMA, RxRingEntry, TxRingEntry},
         hal::gpio::GpioExt,
         mac::Speed,
         Parts,
     };
-
     use core::hash::Hasher;
     use smoltcp::{
         iface::{self, Interface, SocketHandle, SocketSet},
         socket::dhcpv4,
         wire::{EthernetAddress, IpCidr},
     };
-
     use super::NetworkStorage;
 
     #[local]
@@ -275,35 +157,26 @@ mod app {
 
         interface.poll(now_fn(), &mut &mut dma, &mut socket_set);
 
-        if let Ok(mut phy) = EthernetPhy::from_miim(mac, 0) {
-            defmt::println!(
-                "Resetting PHY as an extra step. Type: {}",
-                phy.ident_string()
-            );
+        let mut phy = ieee802_3_miim::phy::LAN8742A::new(mac, 0);
+        phy.phy_init();
 
-            phy.phy_init();
+        // LAN8742A has an interrupt for link up, but Nucleo doesn't wire
+        // it to anything
+        defmt::println!("Waiting for link up.");
+        while !phy.link_established() {}
 
-            defmt::println!("Waiting for link up.");
+        defmt::println!("Link up.");
 
-            while !phy.phy_link_up() {}
-
-            defmt::println!("Link up.");
-
-            if let Some(speed) = phy.speed().map(|s| match s {
-                PhySpeed::HalfDuplexBase10T => Speed::HalfDuplexBase10T,
-                PhySpeed::FullDuplexBase10T => Speed::FullDuplexBase10T,
-                PhySpeed::HalfDuplexBase100Tx => Speed::HalfDuplexBase100Tx,
-                PhySpeed::FullDuplexBase100Tx => Speed::FullDuplexBase100Tx,
-            }) {
-                phy.get_miim().set_speed(speed);
-                defmt::println!("Detected link speed: {}", speed);
-            } else {
-                defmt::warn!("Failed to detect link speed.");
-            }
+        if let Some(speed) = phy.link_speed().map(|s| match s {
+            PhySpeed::HalfDuplexBase10T => Speed::HalfDuplexBase10T,
+            PhySpeed::FullDuplexBase10T => Speed::FullDuplexBase10T,
+            PhySpeed::HalfDuplexBase100Tx => Speed::HalfDuplexBase100Tx,
+            PhySpeed::FullDuplexBase100Tx => Speed::FullDuplexBase100Tx,
+        }) {
+            phy.get_miim().set_speed(speed);
+            defmt::println!("Detected link speed: {}", speed);
         } else {
-            defmt::println!(
-                "Not resetting unsupported PHY. Cannot detect link speed."
-            );
+            defmt::warn!("Failed to detect link speed.");
         }
 
         let dhcp_socket = dhcpv4::Socket::new();
