ff56e189947028d6a3df0fbe1e83b5dfeec7bc9c
Peter Hartley <pdh@utter.chaos.org.uk>
2024-04-11 19:46:26 +0100

    W5500macraw actually DHCP's

--- cross/rp2040-w5500/src/bin/rp2040-w5500macraw-dhcp-rtic.rs
+++ cross/rp2040-w5500/src/bin/rp2040-w5500macraw-dhcp-rtic.rs
@@ -8,10 +8,20 @@ use defmt_rtt as _; // global logger
 use defmt_rtt as _;
 use panic_probe as _;
 use rp_pico as _; // includes boot2
+use smoltcp::iface::{self, SocketStorage};
 
 struct Buffer {
     in_use: bool,
     bytes: [u8; 1536],
+}
+
+impl Buffer {
+    pub const fn new() -> Self {
+        Buffer {
+            in_use: false,
+            bytes: [0u8; 1536],
+        }
+    }
 }
 
 struct RawDevice<Spi: w5500::bus::Bus> {
@@ -20,7 +30,8 @@ struct RawDevice<Spi: w5500::bus::Bus> {
     tx: Buffer,
 }
 
-struct EthTxToken<'a> {
+struct EthTxToken<'a, Spi: w5500::bus::Bus> {
+    w5500: &'a mut w5500::raw_device::RawDevice<Spi>,
     buffer: &'a mut Buffer,
 }
 
@@ -31,28 +42,45 @@ struct EthRxToken<'a> {
 
 impl<Spi: w5500::bus::Bus> smoltcp::phy::Device for RawDevice<Spi> {
     type RxToken<'token> = EthRxToken<'token> where Self: 'token;
-    type TxToken<'token> = EthTxToken<'token> where Self: 'token;
+    type TxToken<'token> = EthTxToken<'token, Spi> where Self: 'token;
 
-    fn receive(&mut self, _timestamp: smoltcp::time::Instant) -> Option<(Self::RxToken<'_>,
-                                                          Self::TxToken<'_>)> {
+    fn receive(
+        &mut self,
+        _timestamp: smoltcp::time::Instant,
+    ) -> Option<(Self::RxToken<'_>, Self::TxToken<'_>)> {
         if !self.tx.in_use && !self.rx.in_use {
             if let Ok(n) = self.w5500.read_frame(&mut self.rx.bytes) {
                 if n > 0 {
                     self.rx.in_use = true;
                     self.tx.in_use = true;
-                    return Some((EthRxToken { count: n, buffer: &mut self.rx },
-                          EthTxToken { buffer: &mut self.tx }));
+                    return Some((
+                        EthRxToken {
+                            count: n,
+                            buffer: &mut self.rx,
+                        },
+                        EthTxToken {
+                            w5500: &mut self.w5500,
+                            buffer: &mut self.tx,
+                        },
+                    ));
                 }
             }
         }
         None
     }
 
-    fn transmit(&mut self, _timestamp: smoltcp::time::Instant) -> Option<Self::TxToken<'_>> {
+    fn transmit(
+        &mut self,
+        _timestamp: smoltcp::time::Instant,
+    ) -> Option<Self::TxToken<'_>> {
         if !self.tx.in_use {
             self.tx.in_use = true;
-            Some(EthTxToken { buffer: &mut self.tx })
+            Some(EthTxToken {
+                w5500: &mut self.w5500,
+                buffer: &mut self.tx,
+            })
         } else {
+            defmt::println!("TX denied");
             None
         }
     }
@@ -62,7 +90,6 @@ impl<Spi: w5500::bus::Bus> smoltcp::phy::Device for RawDevice<Spi> {
         caps.max_transmission_unit = 1536;
         caps.medium = smoltcp::phy::Medium::Ethernet;
         caps.max_burst_size = Some(1);
-        caps.checksum = smoltcp::phy::ChecksumCapabilities::ignored();
         caps
     }
 }
@@ -72,29 +99,52 @@ impl<'a> smoltcp::phy::RxToken for EthRxToken<'a> {
     where
         F: FnOnce(&mut [u8]) -> R,
     {
+        defmt::println!("RX2 {}", self.count);
         let result = f(&mut self.buffer.bytes[0..self.count]);
         self.buffer.in_use = false;
         result
     }
 }
 
-impl<'a> smoltcp::phy::TxToken for EthTxToken<'a> {
+impl<'a> Drop for EthRxToken<'a> {
+    fn drop(&mut self) {
+        if self.buffer.in_use {
+            defmt::println!("Dropping unconsumed RX");
+        }
+        self.buffer.in_use = false;
+    }
+}
+
+impl<'a, Spi: w5500::bus::Bus> smoltcp::phy::TxToken for EthTxToken<'a, Spi> {
     fn consume<R, F>(self, len: usize, f: F) -> R
     where
         F: FnOnce(&mut [u8]) -> R,
     {
         let result = f(&mut self.buffer.bytes[0..len]);
-//        self.
+        match self.w5500.write_frame(&self.buffer.bytes[0..len]) {
+            Ok(_) => defmt::println!("TX {} OK", len),
+            _ => defmt::println!("TX not OK"),
+        }
         self.buffer.in_use = false;
         result
     }
 }
 
+impl<'a, Spi: w5500::bus::Bus> Drop for EthTxToken<'a, Spi> {
+    fn drop(&mut self) {
+        if self.buffer.in_use {
+            defmt::println!("Dropping unconsumed TX");
+        }
+        self.buffer.in_use = false;
+    }
+}
+
 #[rtic::app(device = rp_pico::hal::pac, peripherals = true, dispatchers = [ADC_IRQ_FIFO])]
 mod app {
-    use embedded_hal::delay::DelayNs;
-    use embedded_hal_bus::spi::ExclusiveDevice;
-    use embedded_hal_bus::spi::NoDelay;
+    use crate::Buffer;
+    use crate::NetworkStorage;
+    use crate::RawDevice;
+    use cross_rp2040_w5500::{smoltcp::Stack, unique};
     use fugit::ExtU64;
     use rp2040_hal as hal;
     use rp2040_hal::fugit::RateExtU32;
@@ -116,43 +166,6 @@ mod app {
     use rp_pico::XOSC_CRYSTAL_FREQ;
     use systick_monotonic::Systick;
 
-    /*
-     * Getting a real MAC address depends on the rp2040-flash crate, which
-     * at time of writing still uses rp2040-hal 0.9 (everything else uses
-     * 0.10). We can't just link both versions, because they both think
-     * they're in charge of CPU startup, so linking fails with a ton of
-     * duplicate symbols. Fix once rp2040-flash is updated.
-     *
-
-    struct UniqueId(u64, u64);
-
-    fn rp2040_unique_id() -> UniqueId {
-        let mut unique_id = [0u8; 16];
-        unsafe { rp2040_flash::flash::flash_unique_id(&mut unique_id, true) };
-
-        defmt::println!("Unique id {}", unique_id[0..8]);
-        UniqueId(
-            u64::from_le_bytes(unique_id[0..8].try_into().unwrap()),
-            u64::from_le_bytes(unique_id[8..16].try_into().unwrap()),
-        )
-    }
-
-    fn unique_id(rp2040_id: &UniqueId, salt: &[u8]) -> u64 {
-        let mut h =
-            siphasher::sip::SipHasher::new_with_keys(rp2040_id.0, rp2040_id.1);
-        h.write(salt);
-        h.finish()
-    }
-
-    fn mac_address(rp2040_id: &UniqueId) -> [u8; 6] {
-        let mut mac_address = [0u8; 6];
-        let r = unique_id(rp2040_id, b"w5500-mac").to_ne_bytes();
-        mac_address.copy_from_slice(&r[0..6]);
-        mac_address[0] &= 0xFE; // clear multicast bit
-        mac_address[0] |= 2; // set local bit
-        mac_address
-    }*/
-
     #[shared]
     struct Shared {}
 
@@ -169,28 +182,34 @@ mod app {
     type MySpiChipSelect =
         rp2040_hal::gpio::Pin<Gpio17, FunctionSio<SioOutput>, PullDown>;
 
+    type MySpiBus = w5500::bus::FourWire<MySpi0, MySpiChipSelect>;
+
     #[local]
     struct Local {
+        device: RawDevice<MySpiBus>,
+        stack: Stack,
         nvic: cortex_m::peripheral::NVIC,
-//        w5500: W5500<ExclusiveDevice<MySpi0, MySpiChipSelect, NoDelay>>,
         w5500_irq:
             rp2040_hal::gpio::Pin<Gpio21, FunctionSio<SioInput>, PullNone>,
-//        dhcp: DhcpClient<'static>,
     }
 
     #[monotonic(binds = SysTick, default = true)]
     type Monotonic = Systick<1000>;
 
-    #[init(local = [usb_bus: Option<u32> = None])]
+    fn now_fn() -> smoltcp::time::Instant {
+        let time = monotonics::now().duration_since_epoch().ticks();
+        smoltcp::time::Instant::from_millis(time as i64)
+    }
+
+    #[init(local = [ storage: NetworkStorage = NetworkStorage::new() ])]
     fn init(c: init::Context) -> (Shared, Local, init::Monotonics) {
         defmt::println!("Pre-init");
+        let unique_id = unsafe { unique::UniqueFlashId::new() };
+        let mac = unique::mac_address(&unique_id, b"w5500-spi0");
+        defmt::println!("MAC address: {:x}", mac);
 
-        //        let rp2040_id = rp2040_unique_id();
-        //        let mac = mac_address(&rp2040_id);
-        //        defmt::println!("MAC address: {}", mac);
-
-        let mac = w5500::net::MacAddress {
-            octets: [2u8, 1u8, 2u8, 3u8, 4u8, 5u8],
+        let w5500_mac = w5500::net::MacAddress {
+            octets: mac.clone(),
         };
 
         //*******
@@ -212,7 +231,6 @@ mod app {
         .ok()
         .unwrap();
 
-        let mut timer = hal::Timer::new(c.device.TIMER, &mut resets, &clocks);
         let mono = Systick::new(c.core.SYST, clocks.system_clock.freq().raw());
 
         //*******
@@ -255,12 +273,9 @@ mod app {
         );
 
         let bus = w5500::bus::FourWire::new(spi, spi_ncs);
-        let w5500 = w5500::UninitializedDevice::new(bus);
-
-        // NB w5500's macraw mode turns ON MAC filtering, which has the
-        // effect of disabling multicast. Fix this by just using the bus
-        // read and writes directly.
-        let w5500 = w5500.initialize_macraw(mac).unwrap();
+        let w5500 = w5500::UninitializedDevice::new(bus)
+            .initialize_macraw(w5500_mac)
+            .unwrap();
 
         let w5500_irq = pins.gpio21.into_floating_input();
         w5500_irq.set_interrupt_enabled(EdgeLow, true);
@@ -269,69 +284,30 @@ mod app {
             pac::NVIC::unmask(pac::Interrupt::IO_IRQ_BANK0);
         }
 
-        // W5500 wants a `SpiDevice`, but the HAL provides a `SpiBus`.
-        // This is as designed, as this way the W5500 doesn't have to
-        // know whether the bus is shared (common TX/RX, different nCS
-        // per target) or not (just one nCS). For cases like
-        // W5500-EVB-Pico where there really *is* just one nCS and one
-        // target, `ExclusiveDevice` takes a `SpiBus` and trivially
-        // implements `SpiDevice`.
-        /*
-        let spi =
-            embedded_hal_bus::spi::ExclusiveDevice::new_no_delay(spi, spi_ncs);
-        let mut w5500 = W5500::new(spi);
-
-        let _phy_cfg: PhyCfg = 'outer: loop {
-            // sanity check W5500 communications
-            assert_eq!(w5500.version().unwrap(), w5500_dhcp::ll::VERSION);
-
-            // load the MAC address we got from EEPROM
-            w5500.set_shar(&mac).unwrap();
-            debug_assert_eq!(w5500.shar().unwrap(), mac);
-
-            // wait for the PHY to indicate the Ethernet link is up
-            let mut attempts: u32 = 0;
-            defmt::println!("Polling for link up");
-            const PHY_CFG: PhyCfg =
-                PhyCfg::DEFAULT.set_opmdc(OperationMode::FullDuplex10bt);
-            w5500.set_phycfgr(PHY_CFG).unwrap();
-
-            const LINK_UP_POLL_PERIOD_MILLIS: u32 = 100;
-            const LINK_UP_POLL_ATTEMPTS: u32 = 50;
-            loop {
-                let phy_cfg: PhyCfg = w5500.phycfgr().unwrap();
-                if phy_cfg.lnk() == LinkStatus::Up {
-                    break 'outer phy_cfg;
-                }
-                if attempts >= LINK_UP_POLL_ATTEMPTS {
-                    defmt::println!(
-                        "Failed to link up in {} ms",
-                        attempts * LINK_UP_POLL_PERIOD_MILLIS,
-                    );
-                    break;
-                }
-                timer.delay_ms(LINK_UP_POLL_PERIOD_MILLIS);
-                attempts += 1;
-            }
+        let mut device = super::RawDevice {
+            w5500,
+            rx: Buffer::new(),
+            tx: Buffer::new(),
         };
-        defmt::println!("Done link up");
 
-        let seed: u64 = u64::from(cortex_m::peripheral::SYST::get_current())
-            << 32
-            | u64::from(cortex_m::peripheral::SYST::get_current());
+        let mut stack = Stack::new(
+            &mut device,
+            &unique_id,
+            &mac,
+            &mut c.local.storage.sockets[..],
+            now_fn(),
+        );
+        stack.poll(now_fn(), &mut device);
 
-        let dhcp = DhcpClient::new(DHCP_SN, seed, mac, HOSTNAME);
-        dhcp.setup_socket(&mut w5500).unwrap();
+        periodic::spawn_after(2.secs()).unwrap();
 
-        periodic::spawn_after(1.secs()).unwrap();
-*/
         (
             Shared {},
             Local {
+                device: device,
+                stack: stack,
                 nvic: c.core.NVIC,
-//                w5500,
                 w5500_irq,
-//                dhcp,
             },
             init::Monotonics(mono),
         )
@@ -340,28 +316,27 @@ mod app {
     #[task(local = [nvic])]
     fn periodic(_cx: periodic::Context) {
         cortex_m::peripheral::NVIC::pend(pac::Interrupt::IO_IRQ_BANK0);
-        periodic::spawn_after(1.secs()).unwrap();
+        periodic::spawn_after(2.secs()).unwrap();
     }
 
-    #[task(binds = IO_IRQ_BANK0, local = [w5500_irq], priority = 2)]
+    #[task(binds = IO_IRQ_BANK0, local = [w5500_irq, device, stack], priority = 2)]
     fn eth_interrupt(cx: eth_interrupt::Context) {
-        defmt::println!("ETH IRQ");
         let w5500_irq = cx.local.w5500_irq;
-        /*
-        let (w5500, w5500_irq, dhcp) =
-            (cx.local.w5500, cx.local.w5500_irq, cx.local.dhcp);
-        let had_lease = dhcp.has_lease();
-        let now: u32 = monotonics::now()
-            .duration_since_epoch()
-            .to_secs()
-            .try_into()
-            .unwrap();
-        let _spawn_after_secs: u32 = dhcp.process(w5500, now).unwrap();
-
-        if dhcp.has_lease() && !had_lease {
-            defmt::println!("DHCP succeeded! {}", dhcp.leased_ip())
-        }
-         */
         w5500_irq.clear_interrupt(EdgeLow);
+        defmt::println!("ETH IRQ");
+        cx.local.stack.poll(now_fn(), cx.local.device);
+    }
+}
+
+/// All storage required for networking
+pub struct NetworkStorage {
+    pub sockets: [iface::SocketStorage<'static>; 2],
+}
+
+impl NetworkStorage {
+    pub const fn new() -> Self {
+        NetworkStorage {
+            sockets: [SocketStorage::EMPTY; 2],
+        }
     }
 }
---
+++ cross/rp2040-w5500/src/smoltcp.rs
@@ -0,0 +1,93 @@
+use crate::unique;
+use smoltcp::{socket::dhcpv4, wire::IpCidr};
+
+/// A helper container for a TCP/IP stack and some of its metadata
+pub struct Stack {
+    /// The underlying Smoltcp implementation
+    pub interface: smoltcp::iface::Interface,
+    /// Persistent socket data for active sockets
+    pub socket_set: smoltcp::iface::SocketSet<'static>,
+    dhcp_handle: smoltcp::iface::SocketHandle,
+}
+
+impl Stack {
+    /// Construct a new TCP Stack abstraction
+    ///
+    /// From an interface, a MAC address, and some storage for the
+    /// socket metadata.
+    pub fn new<D: smoltcp::phy::Device, U: unique::UniqueId>(
+        device: &mut D,
+        unique: &U,
+        mac_address: &[u8; 6],
+        sockets: &'static mut [smoltcp::iface::SocketStorage<'static>],
+        now: smoltcp::time::Instant,
+    ) -> Stack {
+        let mut config = smoltcp::iface::Config::new(
+            smoltcp::wire::EthernetAddress::from_bytes(mac_address).into(),
+        );
+        config.random_seed = unique.id(b"smoltcp-config-random");
+        let interface = smoltcp::iface::Interface::new(config, device, now);
+        let mut socket_set = smoltcp::iface::SocketSet::new(sockets);
+
+        let mut dhcp_socket = smoltcp::socket::dhcpv4::Socket::new();
+        let mut retry_config = smoltcp::socket::dhcpv4::RetryConfig::default();
+        retry_config.discover_timeout = smoltcp::time::Duration::from_secs(2);
+        retry_config.initial_request_timeout =
+            smoltcp::time::Duration::from_millis(500);
+        retry_config.request_retries = 10;
+        dhcp_socket.set_retry_config(retry_config);
+        let dhcp_handle = socket_set.add(dhcp_socket);
+
+        Stack {
+            interface,
+            socket_set,
+            dhcp_handle,
+        }
+    }
+
+    /// Poll the interface for new packets, then the DHCP socket
+    pub fn poll<D: smoltcp::phy::Device>(
+        &mut self,
+        now: smoltcp::time::Instant,
+        device: &mut D,
+    ) {
+        self.interface.poll(now, device, &mut self.socket_set);
+        self.poll_dhcp();
+    }
+
+    /// Poll the DHCP socket for any updates
+    ///
+    /// Smoltcp's `dhcpv4::Socket` takes care of retrying/rebinding
+    fn poll_dhcp(&mut self) {
+        let socket =
+            self.socket_set.get_mut::<dhcpv4::Socket>(self.dhcp_handle);
+        let event = socket.poll();
+        match event {
+            None => {}
+            Some(dhcpv4::Event::Configured(config)) => {
+                defmt::println!("DHCP config acquired!");
+                defmt::println!("IP address:      {}", config.address);
+
+                self.interface.update_ip_addrs(|addrs| {
+                    addrs.clear();
+                    addrs.push(IpCidr::Ipv4(config.address)).unwrap();
+                });
+
+                if let Some(router) = config.router {
+                    self.interface
+                        .routes_mut()
+                        .add_default_ipv4_route(router)
+                        .unwrap();
+                } else {
+                    self.interface.routes_mut().remove_default_ipv4_route();
+                }
+            }
+            Some(dhcpv4::Event::Deconfigured) => {
+                defmt::println!("DHCP lost config!");
+                self.interface.update_ip_addrs(|addrs| {
+                    addrs.clear();
+                });
+            }
+        }
+    }
+}
