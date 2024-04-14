b7ecfe43ff2f328a25405a35791978e9887476f9
Peter Hartley <pdh@utter.chaos.org.uk>
2024-04-14 19:54:30 +0100

    Add some system-tests for RP2040-W5500

--- systemtests/tests/device/main.rs
+++ systemtests/tests/device/main.rs
@@ -1,8 +1,11 @@
-#[cfg(feature = "stm32f746-nucleo")]
+#[cfg(feature = "arm")]
 mod device_test;
 
-#[cfg(feature = "stm32f746-nucleo")]
+#[cfg(feature = "arm")]
 mod ssdp_test;
 
 #[cfg(feature = "stm32f746-nucleo")]
 mod stm32f746_nucleo;
+
+#[cfg(feature = "rp2040-w5500")]
+mod rp2040_w5500;
---
+++ systemtests/tests/device/rp2040_w5500.rs
@@ -0,0 +1,54 @@
+use serial_test::*;
+use std::panic;
+use std::time::Duration;
+use crate::device_test::{device_test, DeviceTest};
+
+fn rp2040_test<F: FnOnce(DeviceTest) -> () + panic::UnwindSafe>(
+    firmware: &str,
+    f: F,
+) {
+    device_test(
+        "RP2040",
+        "COTTON_PROBE_RP2040_W5500",
+        firmware,
+        f,
+    );
+}
+
+#[test]
+#[serial(rp2040)]
+#[cfg_attr(miri, ignore)]
+fn arm_rp2040_w5500_hello() {
+    rp2040_test(
+        "../cross/rp2040-w5500/target/thumbv6m-none-eabi/debug/hello",
+        |t| {
+            t.expect("Hello RP2040", Duration::from_secs(25));
+        },
+    );
+}
+
+#[test]
+#[serial(rp2040)]
+#[cfg_attr(miri, ignore)]
+fn arm_rp2040_w5500_dhcp_rtic() {
+    rp2040_test(
+        "../cross/rp2040-w5500/target/thumbv6m-none-eabi/debug/rp2040-w5500-dhcp-rtic",
+        |t| {
+            t.expect_stderr("Finished in", Duration::from_secs(45));
+            t.expect("DHCP succeeded!", Duration::from_secs(10));
+        },
+    );
+}
+
+#[test]
+#[serial(rp2040)]
+#[cfg_attr(miri, ignore)]
+fn arm_rp2040_w5500macraw_dhcp_rtic() {
+    rp2040_test(
+        "../cross/rp2040-w5500/target/thumbv6m-none-eabi/debug/rp2040-w5500macraw-dhcp-rtic",
+        |t| {
+            t.expect_stderr("Finished in", Duration::from_secs(45));
+            t.expect("DHCP config acquired!", Duration::from_secs(10));
+        },
+    );
+}
